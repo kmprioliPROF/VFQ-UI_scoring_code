4c92aa98dced7a62bb6702061a0f57e174f30a37
Katherine M. Prioli <katherine.prioli@rutgers.edu>
2019-09-13 06:53:53 -0400

    Refactoring code; has been revalidated

--- VFQ-UI scoring.R
+++ VFQ-UI scoring.R
@@ -1,6 +1,6 @@
 # https://github.com/kmprioliPROF
 # Code to score VFQ-UI from NEI-VFQ-25
-# Last updated Tue Aug 14 11:28:13 2018 ------------------------------
+# Fri Sep 13 06:45:02 2019 ------------------------------
 
 
 ###########################################################################################################
@@ -18,167 +18,111 @@
 
 #### Load libraries ----
 
-library(here)
-library(tidyverse)
+library(here)         # For nonstatic file paths
+library(tidyverse)    # For importing and tidying data
+library(lubridate)    # For date manipulation
 
 
 #### Import dataset ----
 
-VFQdata <- read_csv(here::here("rawVFQdata.csv")) # Modify path here if desired
+VFQraw <- read_csv(here::here("rawVFQdata.csv")) # Modify path here if desired
 
 
 #### Subset columns, calculate age, and select scorable records ----
 
-VFQdata <- select(VFQdata, record_id, neivfq_date, dob, vfq25_q6, vfq25_q11, vfq25_q14, vfq25_q18, vfq25_q20, vfq25_q25)
-VFQdata$neivfq_date <- lubridate::mdy(VFQdata$neivfq_date)
-VFQdata$dob <- lubridate::mdy(VFQdata$dob)
-VFQdata <- mutate(VFQdata, age = round(as.numeric((neivfq_date - dob) / 365.25), 2))
-VFQdata <- mutate(VFQdata, VFQ_scorable = age + as.numeric(vfq25_q6) + as.numeric(vfq25_q11) + 
-                    as.numeric(vfq25_q14) + as.numeric(vfq25_q18) + as.numeric(vfq25_q20) + 
-                    as.numeric(vfq25_q25))
+VFQdata <- VFQraw %>% 
+  select(record_id, neivfq_date, dob, vfq25_q6, vfq25_q11, vfq25_q14, vfq25_q18, vfq25_q20, vfq25_q25) %>% 
+  mutate(neivfq_date = mdy(neivfq_date),
+         dob = mdy(dob),
+         age = round(as.numeric((neivfq_date - dob) / 365.25), 2),
+         VFQ_scorable = age + as.numeric(vfq25_q6) + as.numeric(vfq25_q11) + as.numeric(vfq25_q14) + 
+           as.numeric(vfq25_q18) + as.numeric(vfq25_q20) + as.numeric(vfq25_q25))
 VFQdata$VFQ_scorable <- with(VFQdata, ifelse(!is.na(VFQ_scorable), 1, 0))
 VFQdata <- filter(VFQdata, VFQ_scorable == 1)
 
 
-#### Scoring Step 1:  Recode ----
+#### Scoring Step 1:  Recoding the VFQ subitems that are used to calculate VFQ-UI ----
 
-VFQdata <- mutate(VFQdata, VFQ6_recode = vfq25_q6)
-VFQdata$VFQ6_recode[VFQdata$VFQ6_recode == 6] <- 5
-
-VFQdata <- mutate(VFQdata, VFQ11_recode = vfq25_q11)
-VFQdata$VFQ11_recode[VFQdata$VFQ11_recode == 6] <- 5
-
-VFQdata <- mutate(VFQdata, VFQ14_recode = vfq25_q14)
-VFQdata$VFQ14_recode[VFQdata$VFQ14_recode == 6] <- 5
-
-VFQdata <- mutate(VFQdata, VFQ18_recode = vfq25_q18)
-VFQdata <- VFQdata %>% mutate(
-  VFQ18_recode = 
-    case_when(
-      VFQ18_recode == 1 ~ 5,
-      VFQ18_recode == 2 ~ 4,
-      VFQ18_recode == 3 ~ 3,
-      VFQ18_recode == 4 ~ 2,
-      VFQ18_recode == 5 ~ 1
-    )
-)
-
-VFQdata <- mutate(VFQdata, VFQ20_recode = vfq25_q20)
-VFQdata <- VFQdata %>% mutate(
-  VFQ20_recode = 
-    case_when(
-      VFQ20_recode == 1 ~ 5,
-      VFQ20_recode == 2 ~ 4,
-      VFQ20_recode == 3 ~ 3,
-      VFQ20_recode == 4 ~ 2,
-      VFQ20_recode == 5 ~ 1
-    )
-)
-
-VFQdata <- mutate(VFQdata, VFQ25_recode = vfq25_q25)
-VFQdata <- VFQdata %>% mutate(
-  VFQ25_recode = 
-    case_when(
-      VFQ25_recode == 1 ~ 5,
-      VFQ25_recode == 2 ~ 4,
-      VFQ25_recode == 3 ~ 3,
-      VFQ25_recode == 4 ~ 2,
-      VFQ25_recode == 5 ~ 1
-    )
-)
-
-
-#### Scoring Step 2:  Compute theta scores ----
-
-VFQdata <- mutate(VFQdata, theta6 = VFQ6_recode)
 VFQdata <- VFQdata %>% 
   mutate(
-    theta6 =
-      case_when(
-         theta6 == 1 ~ -0.8296,
-         theta6 == 2 ~ -0.3246,
-         theta6 == 3 ~ -0.1918,
-         theta6 == 4 ~ -0.1226,
-         theta6 == 5 ~ 0
-      )
-  )
+    VFQ6_recode = case_when(
+      vfq25_q6 == 6 ~ 5,
+      TRUE ~ vfq25_q6),
+    VFQ11_recode = case_when(
+      vfq25_q11 == 6 ~ 5,
+      TRUE ~ vfq25_q11),
+    VFQ14_recode = case_when(
+      vfq25_q14 == 6 ~ 5,
+      TRUE ~ vfq25_q14),
+    VFQ18_recode = case_when(
+      vfq25_q18 == 1 ~ 5,
+      vfq25_q18 == 2 ~ 4,
+      vfq25_q18 == 3 ~ 3,
+      vfq25_q18 == 4 ~ 2,
+      vfq25_q18 == 5 ~ 1),
+    VFQ20_recode = case_when(
+      vfq25_q20 == 1 ~ 5,
+      vfq25_q20 == 2 ~ 4,
+      vfq25_q20 == 3 ~ 3,
+      vfq25_q20 == 4 ~ 2,
+      vfq25_q20 == 5 ~ 1),
+    VFQ25_recode = case_when(
+      vfq25_q25 == 1 ~ 5,
+      vfq25_q25 == 2 ~ 4,
+      vfq25_q25 == 3 ~ 3,
+      vfq25_q25 == 4 ~ 2,
+      vfq25_q25 == 5 ~ 1))
 
-VFQdata <- mutate(VFQdata, theta11 = VFQ11_recode)
+
+#### Scoring Step 2:  Computing theta scores ----
+
 VFQdata <- VFQdata %>% 
   mutate(
-    theta11 = 
-      case_when(
-        theta11 == 1 ~ -0.5809,
-        theta11 == 2 ~ -0.3172,
-        theta11 == 3 ~ -0.2629,
-        theta11 == 4 ~ -0.1275,
-        theta11 == 5 ~ 0
-      )
-  )
+    theta6 = case_when(
+      VFQ6_recode == 1 ~ -0.8296,
+      VFQ6_recode == 2 ~ -0.3246,
+      VFQ6_recode == 3 ~ -0.1918,
+      VFQ6_recode == 4 ~ -0.1226,
+      VFQ6_recode == 5 ~ 0),
+    theta11 = case_when(
+      VFQ11_recode == 1 ~ -0.5809,
+      VFQ11_recode == 2 ~ -0.3172,
+      VFQ11_recode == 3 ~ -0.2629,
+      VFQ11_recode == 4 ~ -0.1275,
+      VFQ11_recode == 5 ~ 0),
+    theta14 = case_when(
+      VFQ14_recode == 1 ~ -0.6473,
+      VFQ14_recode == 2 ~ -0.3067,
+      VFQ14_recode == 3 ~ -0.2671,
+      VFQ14_recode == 4 ~ -0.1742,
+      VFQ14_recode == 5 ~ 0),
+    theta18 = case_when(
+      VFQ18_recode == 1 ~ -0.5067,
+      VFQ18_recode == 2 ~ -0.1751,
+      VFQ18_recode == 3 ~ -0.1382,
+      VFQ18_recode == 4 ~ -0.0996,
+      VFQ18_recode == 5 ~ 0),
+    theta20 = case_when(
+      VFQ20_recode == 1 ~ -0.4555,
+      VFQ20_recode == 2 ~ -0.2172,
+      VFQ20_recode == 3 ~ -0.1932,
+      VFQ20_recode == 4 ~ -0.1447,
+      VFQ20_recode == 5 ~ 0),
+    theta25 = case_when(
+      VFQ25_recode == 1 ~ -0.3692,
+      VFQ25_recode == 2 ~ -0.1485,
+      VFQ25_recode == 3 ~ -0.1561,
+      VFQ25_recode == 4 ~ -0.0924,
+      VFQ25_recode == 5 ~ 0),
+    thetascore = 2.6387 + theta6 + theta11 + theta14 + theta18 + theta20 + theta25)
 
-VFQdata <- mutate(VFQdata, theta14 = VFQ14_recode)
+
+#### Scoring Step 3:  Calculating utility scores ----
+
 VFQdata <- VFQdata %>% 
-  mutate(
-    theta14 = 
-      case_when(
-        theta14 == 1 ~ -0.6473,
-        theta14 == 2 ~ -0.3067,
-        theta14 == 3 ~ -0.2671,
-        theta14 == 4 ~ -0.1742,
-        theta14 == 5 ~ 0
-      )
-  )
-
-VFQdata <- mutate(VFQdata, theta18 = VFQ18_recode)
-VFQdata <- VFQdata %>% 
-  mutate(
-    theta18 = 
-      case_when(
-        theta18 == 1 ~ -0.5067,
-        theta18 == 2 ~ -0.1751,
-        theta18 == 3 ~ -0.1382,
-        theta18 == 4 ~ -0.0996,
-        theta18 == 5 ~ 0
-      )
-  )
-
-VFQdata <- mutate(VFQdata, theta20 = VFQ20_recode)
-VFQdata <- VFQdata %>% 
-  mutate(
-    theta20 = 
-      case_when(
-        theta20 == 1 ~ -0.4555,
-        theta20 == 2 ~ -0.2172,
-        theta20 == 3 ~ -0.1932,
-        theta20 == 4 ~ -0.1447,
-        theta20 == 5 ~ 0
-      )
-  )
-
-VFQdata <- mutate(VFQdata, theta25 = VFQ25_recode)
-VFQdata <- VFQdata %>% 
-  mutate(
-    theta25 = 
-      case_when(
-        theta25 == 1 ~ -0.3692,
-        theta25 == 2 ~ -0.1485,
-        theta25 == 3 ~ -0.1561,
-        theta25 == 4 ~ -0.0924,
-        theta25 == 5 ~ 0
-      )
-  )
-
-VFQdata %>%
-  add_column(thetascore = 0)
-VFQdata <- mutate(VFQdata, thetascore = 2.6387 + theta6 + theta11 + theta14 + theta18 + theta20 + theta25)
+  mutate(VFQ_UI = 0.87397 + (0.0009 * age) + (-0.10619 * thetascore) + (-0.11218 * thetascore^2) + (0.02779 * thetascore^3))
 
 
-#### Scoring Step 3:  Calculate utility scores ----
-
-VFQdata <- mutate(VFQdata, VFQ_UI = 0.87397 + (0.0009 * age) + (-0.10619 * thetascore) + 
-                    (-0.11218 * thetascore^2) + (0.02779 * thetascore^3))
-
-
-#### Write results to .csv ----
+#### Writing results to .csv ----
 
 write.csv(VFQdata, here::here("VFQ-UI_scored.csv")) # Modify path here if desired
